d64001c518445da7076b376a5d08b59150e015b8
Domagoj Kulundžić <domagoj1989@gmail.com>
2014-04-13 23:36:39 +0200

    Added Test class.

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Domagoj Kulundžić on 4/13/14.
+//  Copyright (c) 2014 Domagoj Kulundžić. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Domagoj Kulundžić on 4/13/14.
+//  Copyright (c) 2014 Domagoj Kulundžić. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
